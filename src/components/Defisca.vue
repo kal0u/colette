3d2458e14b5fcd300709ba4016a8d8e2cbeb967d
Kal0u <rome.bastien@gmail.com>
2021-03-02 15:18:44 +0100

    Update package-lock.json, Defisca.vue, and 2 more files...

--- src/components/Defisca.vue
+++ src/components/Defisca.vue
@@ -1,15 +1,41 @@
 <template>
   <v-container>
+    <!-- <div class="textCont" style="width: 750px; margin: 0 auto"> -->
     <div class="textCont">
-      <h2>Entreprises, investissez dans de l’art</h2>
-      <p class="mt-6">
-        Faites entrer une oeuvre d’art<br />
-        dans vos locaux et déduisez<br />
-        sa valeur de vos impôts.<br />
-        SAS, SARL, EI, BNC, BIC, etc...<br />
-        Financez facilement grâce à l’achat<br />
-        ou au leasing.
-      </p>
+      <div class="topComp">
+        <p class="pt-4 ml-2 mr-2 firstText">
+          Exposez une oeuvre d’art <br />dans vos locaux et défiscalisez.
+        </p>
+      </div>
+      <div class="cardContainer">
+        <div class="card1">
+          <p class="title">Sublimez votre image</p>
+          <p class="cardText">
+            Une entreprise qui acquiert de l’art est une entreprise cultivée. En
+            plus d’afficher son soutien aux artistes, votre société bénificiera
+            de tous les avantages que procure l’art : inspiration,
+            communication, événements, esthétique, valeurs...
+          </p>
+        </div>
+        <div class="card2">
+          <p class="title">Brillez, inspirez.</p>
+          <p class="cardText">
+            Offrez à vos collaborateurs, clients ou patients une attention
+            remarquable avec de l’art. Personnalisez vos locaux en les rendant
+            uniques (inspirants, relaxants, ou raffinés...), et faites forte
+            impression.
+          </p>
+        </div>
+        <div class="card3">
+          <p class="title">Payez moins d’impôts.</p>
+          <p class="cardText">
+            En cas d’achat, vous pouvez défiscaliser jusqu’à 100% de la valeur
+            de l’oeuvre pendant 5 ans. Si vous optez pour le leasing, déduisez
+            les loyers de votre résultat : acquérir de l’art, c’est réduire vos
+            impôts.
+          </p>
+        </div>
+      </div>
     </div>
   </v-container>
 </template>
@@ -32,9 +58,50 @@ export default Vue.extend({
 .textCont {
   text-align: center;
   width: 100%;
-  height: 100%;
-  background-image: url("../assets/img/cosmos.jpg") ;
-    background-size: cover;
+  height: 1000px;
+  background-image: url("../assets/img/cosmos.jpg");
+  background-size: cover;
+  position: relative;
 }
-
+.topComp {
+  position: absolute;
+  top: 15%;
+  left: 35%;
+  background-color: white;
+  border-radius: 25px;
+}
+.firstText {
+  font-size: 25px;
+  font-weight: bold;
+}
+.card1 {
+  background-color: white;
+  /* position: relative;
+  top: 55%;
+  left: 5%; */
+  width: 30%;
+  border-radius: 25px;
+}
+.card2 {
+  background-color: white;
+  /* position: relative;
+  top: %;
+  left: 35%;*/
+  width: 30%;
+  border-radius: 25px;
+}
+.card3 {
+  background-color: white;
+  /* position: relative;
+  top: %;
+  left: 35%;*/
+  width: 30%;
+  border-radius: 25px;
+}
+.cardContainer {
+  display: flex;
+  justify-content: space-around;
+  position: relative;
+  top: 55%;
+}
 </style>
